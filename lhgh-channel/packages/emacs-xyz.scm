3389ceaf4b5031ab5804049ac95e9b9ee52ac846
luishgh <51053603+luishgh@users.noreply.github.com>
2022-05-31 16:03:04 -0300

    lhgh: Add emacs-lsp-bridge
    * lhgh-channel/packages/emacs-xyz.scm (emacs-lsp-bridge): New variable.

--- lhgh-channel/packages/emacs-xyz.scm
+++ lhgh-channel/packages/emacs-xyz.scm
@@ -98,3 +98,39 @@ Org-Roam-UI is meant a successor of org-roam-server
 that extends functionality of org-roam with a Web app that runs
 side-by-side with Emacs.")
     (license license:gpl3)))
+
+(define-public emacs-lsp-bridge
+  (let ((version "0.5") (commit "3b6e187215c5897055e97ea160198b963f525172")
+        (revision "0"))
+    (package
+      (name "emacs-lsp-bridge")
+      (version (git-version version revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/manateelazycat/lsp-bridge")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "0h6s5nxv19n0p9lm8a13xmq1rb2xl9i10c3j006xgrzncaphgbpc"))))
+      (build-system emacs-build-system)
+      ;; NOTE: needed for tests
+      (native-inputs (list emacs-orderless))
+      ;; NOTE: emacs-all-the-icons is technically also optional (only
+      ;; needed for tests), but i don't know if its really worth it to
+      ;; not propagate it
+      (propagated-inputs (list emacs-markdown-mode emacs-all-the-icons
+                               emacs-posframe))
+      (home-page "https://github.com/manateelazycat/lsp-bridge")
+      ;; FIXME: when porting to guix proper, this probably isn't a good synopsis
+      (synopsis "Fastest LSP client for Emacs")
+      (description
+       "Lsp-bridge's goal is to become the fastest LSP
+client in Emacs.
+
+Lsp-bridge uses python's threading technology to build caches that
+bridge Emacs and LSP server.  Lsp-bridge will provide smooth completion
+experience without compromise to slow down emacs' performance.")
+      ;; FIXME: The man simply didn't include a License file
+      (license license:expat))))
